75104b6a1b6522649acb35b2b981b844a73e2251
jimmiafriandoakbar <jimmiafriandoakbar@gmail.com>
2026-06-17 16:42:32 +0700

    mail v1

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SIPENA-WAWAY - Login</title>
-    <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
+    <link rel="icon" type="image/png" href="{{ asset('Logo.png') }}">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
